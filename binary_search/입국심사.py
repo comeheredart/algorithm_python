c1e973bdb8c19f010a5a7d6b1aeede07a7510036
Jen <wonee0321@gmail.com>
2021-07-22 20:34:56 +0900

    입국심사

---
+++ binary_search/입국심사.py
@@ -0,0 +1,23 @@
+#https://programmers.co.kr/learn/courses/30/lessons/43238
+# 입국 심사
+
+
+def solution(n, times):
+    answer = 0
+    left, right = 1, max(times) * n
+    
+    while left <= right:
+        mid = (left + right) // 2
+        people = 0
+        
+        for i in times:
+            people += mid // i
+            
+            if people >= n:
+                answer = mid
+                right = mid - 1
+                break
+                
+        if people < n:
+            left = mid + 1
+    return answer
