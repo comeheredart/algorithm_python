0187542fe50b89b3ed2f9f271784ea33bfbca238
Jen <wonee0321@gmail.com>
2021-11-18 13:08:53 +0900

    위장.py

---
+++ math/위장.py
@@ -0,0 +1,18 @@
+#https://programmers.co.kr/learn/courses/30/lessons/42578
+
+def solution(clothes):
+  closet = {}
+  result = 1
+    
+  for element in clothes:
+      key = element[1]
+      value = element[0]
+      if key in closet:
+          closet[key].append(value)
+      else:
+          closet[key] = [value]
+            
+    
+  for key in closet.keys():
+      result = result * (len(closet[key]) + 1)
+  return result - 1
